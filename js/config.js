767c8d52441448eb498f9697f00cfdac2d8ae73f
Bassel Attia <40627412+bassel27@users.noreply.github.com>
2025-05-28 19:12:18 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_v4wU35Jus', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6hmr247fedset0a0aelifnnt54', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2r6q2acghfk0hd4jbat1295lu3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1'
     },
     api: {
